b3f0ed098f5e4c7caac29cadd61d8e21cb4bf78b
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-09-12 20:59:28 +0200

    Update .scalafmt.conf and PlantUMLPlugin.scala (#86)
    * Update .scalafmt.conf and PlantUMLPlugin.scala
    
    Handle not existing plantuml sources
    
    * Refactor PlantUMLPlugin trigger to noTrigger

--- plugin/src/main/scala/plantuml/PlantUMLPlugin.scala
+++ plugin/src/main/scala/plantuml/PlantUMLPlugin.scala
@@ -6,11 +6,12 @@ import sbt.Keys._
 import net.sourceforge.plantuml.SourceFileReader
 
 import scala.collection.JavaConverters._
+import java.nio.file.Files
 
 object PlantUMLPlugin extends AutoPlugin {
   override def requires: Plugins = plugins.JvmPlugin
 
-  override def trigger: PluginTrigger = allRequirements
+  override def trigger: PluginTrigger = noTrigger
 
   object autoImport {
     val plantUMLSource = settingKey[File]("plantUML sources")
@@ -28,11 +29,18 @@ object PlantUMLPlugin extends AutoPlugin {
     resourceGenerators.in(Compile) += Def
       .task[Seq[File]] {
         val logger = streams.value.log
-        logger.info(s"Generating PlantUML diagrams from ${plantUMLSource.value}")
+        logger.info(
+          s"Generating PlantUML diagrams from ${plantUMLSource.value}"
+        )
         val source = plantUMLSource.value.toPath()
-        val outputBaseDir = target.value / plantUMLTarget.in(Compile).value
-        logger.info(s"Generating PlantUML diagrams to ${outputBaseDir}")
-        PlantUmlWalker.genAllPngs(source, outputBaseDir.toPath()).toList
+        if (Files.exists(source)) {
+          val outputBaseDir = target.value / plantUMLTarget.in(Compile).value
+          logger.info(s"Generating PlantUML diagrams to ${outputBaseDir}")
+          PlantUmlWalker.genAllPngs(source, outputBaseDir.toPath()).toList
+        } else {
+          logger.info(s"PlantUML source directory ${source} does not exist")
+          List.empty
+        }
       }
       .taskValue
   )
